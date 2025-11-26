c95efefb968ff0ea704d87f3b511cc16a31f2fa7
Everlyn ` Amethyst <186914412+everlynnameyhst@users.noreply.github.com>
2025-11-26 20:09:10 +0700

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -142,7 +142,7 @@ ${formData.message}
                   {/* Logo image dengan gradient overlay */}
                   <div className="relative w-full h-full">
                     <Image
-                      src="https://lannytourl.vestia.icu/api/file/69264e03ef0f1355a89d2013.png"
+                      src="https://lannytourl.vestia.icu/api/file/6926fa14fdcdd67351cb6d0c.jpg"
                       alt="Contact Logo"
                       fill
                       className="object-cover scale-110"
