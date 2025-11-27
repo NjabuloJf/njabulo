17d06d8efe69f2b6954016da061f9c7ccfdee64d
Everlyn ` Amethyst <186914412+everlynnameyhst@users.noreply.github.com>
2025-11-27 07:22:50 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+/**
+ * Create By Everlyn ` Amyhst.
+ * Contact Me on wa.me/17426664866
+ * Follow https://github.com/everlynnameyhst
+ */
+
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
